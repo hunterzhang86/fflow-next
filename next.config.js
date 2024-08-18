0d23e5cc595d2d91249913681d3d97fdb9e62804
hunterzhang86 <hunterzhang86@gmail.com>
2024-08-18 23:04:51 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 const { withContentlayer } = require("next-contentlayer2");
 
 import("./env.mjs");
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-
-export default nextConfig;
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
