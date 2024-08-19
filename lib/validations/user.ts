15e3c525c094a834355b1c0d51d22d84ed54b923
张欢 <zhanghuan2@linklogis.com>
2024-08-19 21:53:15 +0800

    update

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -1,10 +1,5 @@
 import * as z from "zod";
-import { UserRole } from "@/types";
 
 export const userNameSchema = z.object({
   name: z.string().min(3).max(32),
-});
-
-export const userRoleSchema = z.object({
-  role: z.nativeEnum(UserRole),
 });
