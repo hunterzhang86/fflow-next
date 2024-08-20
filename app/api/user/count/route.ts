9ddf83f1ddbd00a852ee3a9f3b9ae353bac480f8
张欢 <zhanghuan2@linklogis.com>
2024-08-20 13:33:24 +0800

    test

--- app/api/user/count/route.ts
+++ app/api/user/count/route.ts
@@ -2,6 +2,6 @@ import { countUsers } from "@/lib/user";
 
 export async function GET(req: Request) {
   const count = await countUsers();
-  console.log("user count:", count);
+  console.log("#### user count:", count);
   return new Response(count.toString(), { status: 200 });
 }
