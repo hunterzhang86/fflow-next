3ad90a8e815e364c270d641e5493def53bac81cd
张欢 <zhanghuan2@linklogis.com>
2024-09-03 22:04:50 +0800

    修复打包的问题

--- src/app/[locale]/(marketing)/layout.tsx
+++ src/app/[locale]/(marketing)/layout.tsx
@@ -1,20 +1,24 @@
+import { unstable_setRequestLocale } from "next-intl/server";
+
+import { NavMobile } from "@/components/layout/mobile-nav";
 import { NavBar } from "@/components/layout/navbar";
 import { SiteFooter } from "@/components/layout/site-footer";
-import { NavMobile } from "@/components/layout/mobile-nav";
-import {unstable_setRequestLocale} from 'next-intl/server';
 
 interface MarketingLayoutProps {
   children: React.ReactNode;
   params: { locale: string };
 }
 
-export default function MarketingLayout({ children, params: {locale} }: MarketingLayoutProps) {
+export default function MarketingLayout({
+  children,
+  params: { locale },
+}: MarketingLayoutProps) {
   unstable_setRequestLocale(locale);
 
   return (
     <div className="flex min-h-screen flex-col">
-      <NavMobile params={{ locale }} />
-      <NavBar scroll={true} params={{ locale }} />
+      <NavMobile />
+      <NavBar scroll={true} />
       <main className="flex-1">{children}</main>
       <SiteFooter />
     </div>
