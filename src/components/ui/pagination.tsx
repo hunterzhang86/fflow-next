e1b122e6a9c156873ebbaacfc344fab72e423f48
张欢 <zhanghuan2@linklogis.com>
2024-09-05 19:25:48 +0800

    修复代码扫描问题

--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -68,39 +68,39 @@ export function Pagination({ pagination, setPagination }: PaginationProps) {
         <div className="flex items-center space-x-2">
           <Button
             variant="outline"
-            className="hidden h-8 w-8 p-0 lg:flex"
+            className="hidden size-8 p-0 lg:flex"
             onClick={() => handlePageChange(1)}
             disabled={pagination.current === 1}
           >
             <span className="sr-only">Go to first page</span>
-            <ChevronsLeft className="h-4 w-4" />
+            <ChevronsLeft className="size-4" />
           </Button>
           <Button
             variant="outline"
-            className="h-8 w-8 p-0"
+            className="size-8 p-0"
             onClick={() => handlePageChange(pagination.current - 1)}
             disabled={pagination.current === 1}
           >
             <span className="sr-only">Go to previous page</span>
-            <ChevronLeftIcon className="h-4 w-4" />
+            <ChevronLeftIcon className="size-4" />
           </Button>
           <Button
             variant="outline"
-            className="h-8 w-8 p-0"
+            className="size-8 p-0"
             onClick={() => handlePageChange(pagination.current + 1)}
             disabled={pagination.current === totalPages}
           >
             <span className="sr-only">Go to next page</span>
-            <ChevronRightIcon className="h-4 w-4" />
+            <ChevronRightIcon className="size-4" />
           </Button>
           <Button
             variant="outline"
-            className="hidden h-8 w-8 p-0 lg:flex"
+            className="hidden size-8 p-0 lg:flex"
             onClick={() => handlePageChange(totalPages)}
             disabled={pagination.current === totalPages}
           >
             <span className="sr-only">Go to last page</span>
-            <ChevronsRight className="h-4 w-4" />
+            <ChevronsRight className="size-4" />
           </Button>
         </div>
       </div>
