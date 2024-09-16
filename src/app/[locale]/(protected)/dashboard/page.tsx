697d35deb25524228ae6b130ecf2977da1e4c06e
张欢 <zhanghuan2@linklogis.com>
2024-09-16 16:35:13 +0800

    feat: dashboard 实现多语言

--- src/app/[locale]/(protected)/dashboard/page.tsx
+++ src/app/[locale]/(protected)/dashboard/page.tsx
@@ -1,31 +1,26 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { redirect } from 'next/navigation';
+
 import { getCurrentUser } from "@/lib/session";
-import { constructMetadata } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { DashboardHeader } from "@/components/dashboard/header";
-import { EmptyPlaceholder } from "@/components/shared/empty-placeholder";
 
-export const metadata = constructMetadata({
-  title: "Dashboard - FFlow Next",
-  description: "Create and manage content.",
-});
+import DashboardPageClient from "./page-client";
 
-export default async function DashboardPage() {
-  const user = await getCurrentUser();
+export default async function DashboardPage({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const messages = await getMessages({ locale });
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    redirect(`/${locale}/login`);
+  }
 
   return (
-    <>
-      <DashboardHeader
-        heading="Dashboard"
-        text={`Current Role : ${user?.role} — Change your role in settings.`}
-      />
-      <EmptyPlaceholder>
-        <EmptyPlaceholder.Icon name="post" />
-        <EmptyPlaceholder.Title>No content created</EmptyPlaceholder.Title>
-        <EmptyPlaceholder.Description>
-          You don&apos;t have any content yet. Start creating content.
-        </EmptyPlaceholder.Description>
-        <Button>Add Content</Button>
-      </EmptyPlaceholder>
-    </>
+    <NextIntlClientProvider messages={messages}>
+      <DashboardPageClient locale={locale} user={currentUser} />
+    </NextIntlClientProvider>
   );
 }
