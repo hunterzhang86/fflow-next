94b05d52095ef8711070f1d3428556443d64abab
zhanghuan <zhanghuan2@linklogis.com>
2024-10-10 16:35:39 +0800

    feat(analytics): 集成Google Analytics
    添加Google Analytics以跟踪网站访问数据
    
    - 在marketing layout中集成GoogleAnalytics组件
    - 添加NEXT_PUBLIC_GOOGLE_ANALYTICS环境变量
    - 安装@next/third-parties依赖包

--- src/app/[locale]/(marketing)/layout.tsx
+++ src/app/[locale]/(marketing)/layout.tsx
@@ -6,6 +6,7 @@ import { SiteFooter } from "@/components/layout/site-footer";
 import { getMarketingConfig } from "@/config/marketing";
 import { useTranslations } from "next-intl";
 import { getDocsConfig } from "@/config/docs";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 interface MarketingLayoutProps {
   children: React.ReactNode;
@@ -31,6 +32,7 @@ export default function MarketingLayout({
 
   return (
     <div className="flex min-h-screen flex-col">
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || ''} />
       <NavMobile marketingConfig={marketingConfig} docsConfig={docsConfig} translations={translations} />
       <NavBar scroll={true} marketingConfig={marketingConfig} docsConfig={docsConfig} translations={translations} />
       <main className="flex-1">{children}</main>
